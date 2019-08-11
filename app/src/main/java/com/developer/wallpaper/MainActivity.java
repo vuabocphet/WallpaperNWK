8de381b25e0661d61d1a84059900bd0708d51ff1
Developer <vuabocphet98@gmail.com>
2019-08-11 20:34:15 +0700

    Code update new 11-8-2019

--- app/src/main/java/com/developer/wallpaper/MainActivity.java
+++ app/src/main/java/com/developer/wallpaper/MainActivity.java
@@ -6,7 +6,6 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.developer.wallpaper.retrofit.APIClient;
--- app/src/main/java/com/developer/wallpaper/retrofit/DataClient.java
+++ app/src/main/java/com/developer/wallpaper/retrofit/DataClient.java
@@ -1,23 +1,25 @@
 package com.developer.wallpaper.retrofit;
 
 import com.developer.wallpaper.ABC;
+import com.developer.wallpaper.model.CategoryModel;
 
 
 import java.util.List;
 
 import retrofit2.Call;
-import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
-import retrofit2.http.Part;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface DataClient {
 
       //Code GET ALL DATA
     @GET("/wp-json/wp/v2/posts?_embed")
     Call<List<ABC>> getDataALL();
+
+    @FormUrlEncoded
+    @GET("wp-json/wp/v2/media/{id}")
+        Call<List<CategoryModel>> getDataCategory(@Path("id")int id);
 
 
       //Code GET CATEGORY POSITION
